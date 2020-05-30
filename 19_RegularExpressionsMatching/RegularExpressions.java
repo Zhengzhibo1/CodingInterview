6034ad512e968f3735c91515d29c68c6792a7114
Zhengzhibo1 <60970579+Zhengzhibo1@users.noreply.github.com>
2020-05-30 15:33:23 +0800

    Update RegularExpressions.java

--- 19_RegularExpressionsMatching/RegularExpressions.java
+++ 19_RegularExpressionsMatching/RegularExpressions.java
@@ -13,86 +13,88 @@ public class RegularExpressions {
 
 // ============算法实现================
 	public boolean match(char[] str, char[] pattern) {
-		if(str == null || pattern == null) {
+		if (str == null || pattern == null) {
 			return false;
 		}
 		int strIndex = 0;
 		int patternIndex = 0;
 		return matchCore(str, strIndex, pattern, patternIndex);
 	}
-	
+
 	public boolean matchCore(char[] str, int strIndex, char[] pattern, int patternIndex) {
-		//str与pattern均到尾部，匹配成功
-		if(strIndex == str.length && patternIndex == pattern.length) {
+		// str与pattern均到尾部，匹配成功
+		if (strIndex == str.length && patternIndex == pattern.length) {
 			return true;
 		}
-		//如果pattern先到尾，匹配失败
-		if(strIndex != str.length && patternIndex == pattern.length) {
+		// 如果pattern先到尾，匹配失败
+		if (strIndex != str.length && patternIndex == pattern.length) {
 			return false;
 		}
-		//模式第2个是*
-		if(patternIndex + 1 < pattern.length && pattern[patternIndex+1] == '*') {
-			if((strIndex < str.length && pattern[patternIndex] == str[strIndex]) || (strIndex < str.length && pattern[patternIndex] == '.')) {
+		// 模式第2个是*
+		if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
+			if ((strIndex < str.length && pattern[patternIndex] == str[strIndex])
+					|| (strIndex < str.length && pattern[patternIndex] == '.')) {
 				return matchCore(str, strIndex + 1, pattern, patternIndex)
 						|| matchCore(str, strIndex + 1, pattern, patternIndex + 2)
 						|| matchCore(str, strIndex, pattern, patternIndex + 2);
-			}else {
+			} else {
 				return matchCore(str, strIndex, pattern, patternIndex + 2);
 			}
 		}
-		//模式第2个不是*
-		if(strIndex < str.length && (pattern[patternIndex] == str[strIndex] || pattern[patternIndex] == '.')) {
+		// 模式第2个不是*
+		if (strIndex < str.length && (pattern[patternIndex] == str[strIndex] || pattern[patternIndex] == '.')) {
 			return matchCore(str, strIndex + 1, pattern, patternIndex + 1);
 		}
-		
+
 		return false;
 	}
 
 // ==================测试代码=========================
-	
-	public void test(String testName, String str, String pattern, boolean expected) {
-	    if(testName != null)
-	        System.out.printf("%s begins: ", testName);
 
-	    char[] string = str.toCharArray();
-	    char[] pat = pattern.toCharArray();
-	    if(match(string, pat) == expected)
-	    	System.out.printf("Passed.\n");
-	    else
-	    	System.out.printf("FAILED.\n");
+	public void test(String testName, String str, String pattern, boolean expected) {
+		if (testName != null)
+			System.out.printf("%s begins: ", testName);
+
+		char[] string = str.toCharArray();
+		char[] pat = pattern.toCharArray();
+		if (match(string, pat) == expected)
+			System.out.printf("Passed.\n");
+		else
+			System.out.printf("FAILED.\n");
 	}
+
 	public static void main(String[] args) {
 		RegularExpressions r = new RegularExpressions();
 		r.test("test01", "", "", true);
-	    r.test("test02", "", ".*", true);
-	    r.test("test03", "", ".", false);
-	    r.test("test04", "", "c*", true);
-	    r.test("test05", "a", ".*", true);
-	    r.test("test06", "a", "a.", false);
-	    r.test("test07", "a", "", false);
-	    r.test("test08", "a", ".", true);
-	    r.test("test09", "a", "ab*", true);
-	    r.test("test10", "a", "ab*a", false);
-	    r.test("test11", "aa", "aa", true);
-	    r.test("test12", "aa", "a*", true);
-	    r.test("test13", "aa", ".*", true);
-	    r.test("test14", "aa", ".", false);
-	    r.test("test15", "ab", ".*", true);
-	    r.test("test16", "ab", ".*", true);
-	    r.test("test17", "aaa", "aa*", true);
-	    r.test("test18", "aaa", "aa.a", false);
-	    r.test("test19", "aaa", "a.a", true);
-	    r.test("test20", "aaa", ".a", false);
-	    r.test("test21", "aaa", "a*a", true);
-	    r.test("test22", "aaa", "ab*a", false);
-	    r.test("test23", "aaa", "ab*ac*a", true);
-	    r.test("test24", "aaa", "ab*a*c*a", true);
-	    r.test("test25", "aaa", ".*", true);
-	    r.test("test26", "aab", "c*a*b", true);
-	    r.test("test27", "aaca", "ab*a*c*a", true);
-	    r.test("test28", "aaba", "ab*a*c*a", false);
-	    r.test("test29", "bbbba", ".*a*a", true);
-	    r.test("test30", "bcbbabab", ".*a*a", false);
+		r.test("test02", "", ".*", true);
+		r.test("test03", "", ".", false);
+		r.test("test04", "", "c*", true);
+		r.test("test05", "a", ".*", true);
+		r.test("test06", "a", "a.", false);
+		r.test("test07", "a", "", false);
+		r.test("test08", "a", ".", true);
+		r.test("test09", "a", "ab*", true);
+		r.test("test10", "a", "ab*a", false);
+		r.test("test11", "aa", "aa", true);
+		r.test("test12", "aa", "a*", true);
+		r.test("test13", "aa", ".*", true);
+		r.test("test14", "aa", ".", false);
+		r.test("test15", "ab", ".*", true);
+		r.test("test16", "ab", ".*", true);
+		r.test("test17", "aaa", "aa*", true);
+		r.test("test18", "aaa", "aa.a", false);
+		r.test("test19", "aaa", "a.a", true);
+		r.test("test20", "aaa", ".a", false);
+		r.test("test21", "aaa", "a*a", true);
+		r.test("test22", "aaa", "ab*a", false);
+		r.test("test23", "aaa", "ab*ac*a", true);
+		r.test("test24", "aaa", "ab*a*c*a", true);
+		r.test("test25", "aaa", ".*", true);
+		r.test("test26", "aab", "c*a*b", true);
+		r.test("test27", "aaca", "ab*a*c*a", true);
+		r.test("test28", "aaba", "ab*a*c*a", false);
+		r.test("test29", "bbbba", ".*a*a", true);
+		r.test("test30", "bcbbabab", ".*a*a", false);
 
 	}
 
